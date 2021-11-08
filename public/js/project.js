ee73d9d273d2d4b77244c4777acc6a718efe4d9e
KungFuni0812 <kainichen407@gmail.com>
2021-11-07 21:04:08 -0500

    updated new looks

--- public/js/project.js
+++ public/js/project.js
@@ -62,7 +62,10 @@ $(document).ready(function() {
                     return `${statusList[data]}`
                 }
             },
-            { "data": "targetsalary" },
+            { 
+                "data": "targetsalary",
+                render: $.fn.dataTable.render.number( ',', '.', 2, '$' )
+            },
             {
                 data: null,
                 orderable: false,
